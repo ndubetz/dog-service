fde765e5ba55707e25a980a012041184b255820b
ndubetz <nate@autonomic.ai>
2018-06-01 18:36:31 -0400

    Initial commit

---
+++ src/main/java/com/integrate/dogservice/DogserviceApplication.java
@@ -0,0 +1,12 @@
+package com.integrate.dogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/integrate/dogservice/DogserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.integrate.dogservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
